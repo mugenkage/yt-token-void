9665494e86d5e3430f1e0ae98977c6bbfebb394c
mugenkage <mugenkage.ai@gmail.com>
2026-06-16 21:48:21 +0530

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,16 @@
+const { google } = require("googleapis");
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+export default function handler(req, res) {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/youtube.upload"],
+    prompt: "consent",
+  });
+  res.redirect(authUrl);
+}
