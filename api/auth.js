eb83326251f7976eab692f33d26878fd814dad41
mugenkage <mugenkage.ai@gmail.com>
2026-06-16 22:15:31 +0530

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
+    response_type: 'code',
+    scope: 'https://www.googleapis.com/auth/youtube',
+    access_type: 'offline',
+    prompt: 'consent',
+  });
+
+  res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params}`);
+}
